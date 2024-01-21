16f9c59814d94eab23265f46f6a0ca0da910561b
Amella <mohamadtomikurniawan@gmail.com>
2024-01-21 23:35:12 +0700

    first commit

---
+++ penduduk.php
@@ -0,0 +1,55 @@
+<?php include "Header.php";?>
+<!-- Awal baris -->
+		<div class="row">
+			<div class="col-md-8"><!-- Awal Kolom Pertama -->
+			<div class="panel panel-default">
+				<div class="panel-body">
+				<h2 style="text-muted"><span class="glyphicon glyphicon-list"></span> Data Alamat Penduduk Desa Kotasan</h2>
+						<table class="table table-bordered table-hover" id="data-table">
+						<thead>
+						<tr>
+							<th>Nama Lengkap</th>
+							<th>Jenis Kelamin</th>
+							<th>Alamat</th>
+						</tr>
+						</thead>
+						<tr>
+							<td>Arief Anugrah</td>
+							<td>Laki-Laki</td>
+							<td>Dusun 5</td>
+						</tr>
+						<tr>
+							<td>Dewi Sartika</td>
+							<td>Perempuan</td>
+							<td>Dusun 6</td>
+						</tr>
+						<tr>
+							<td>Nurul mawaddah</td>
+							<td>Perempuan</td>
+							<td>Dusun 2</td>
+						</tr>
+						<tr>
+							<td>Azizah yuwana</td>
+							<td>Perempuan</td>
+							<td>Dusun 3</td>
+						</tr>
+						<tr>
+							<td>Dini Ariska</td>
+							<td>Perempuan</td>
+							<td>Dusun 1</td>
+						</tr>
+						<tr>
+							<td>Widiarti Kartika</td>
+							<td>Perempuan</td>
+							<td>Dusun 4</td>
+						</tr>
+						<tr>
+							<td>Danu Irwansyah</td>
+							<td>Laki-Laki</td>
+							<td>Dusun 7</td>
+						</tr>
+						</table>
+				</div>
+      </div>
+			</div><!-- Akhir Kolom Pertama -->
+  <?php include "footer.php"; ?>
